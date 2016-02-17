cc015521075673638c981683154793f6b0e6cfb3
JasonH2o <jasonliu820@hotmail.com>
2016-02-17 13:24:46 -0800

    testing 3
    lklsadf

---
+++ TestProductApp/Models/Price.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProductApp.Models
+{
+    public class Price
+    {
+        int abc = 90;
+    }
+}
